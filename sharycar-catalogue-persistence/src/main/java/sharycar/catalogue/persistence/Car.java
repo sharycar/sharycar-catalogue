d9c3880e7c7514cb975c7f6bb3e4fb8b8b14f4f6
Jaka <jkrajnc11@gmail.com>
2018-12-25 13:11:04 +0100

    persistence objects

--- sharycar-catalogue-persistence/src/main/java/sharycar/catalogue/persistence/Car.java
+++ sharycar-catalogue-persistence/src/main/java/sharycar/catalogue/persistence/Car.java
@@ -1,4 +1,57 @@
 package sharycar.catalogue.persistence;
 
+import javax.persistence.*;
+
 public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String year;
+
+    private String brand;
+
+    private String kilometers;
+
+    private String color;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getKilometers() {
+        return kilometers;
+    }
+
+    public void setKilometers(String kilometers) {
+        this.kilometers = kilometers;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
--- sharycar-catalogue-persistence/src/main/java/sharycar/catalogue/persistence/Reservation.java
+++ sharycar-catalogue-persistence/src/main/java/sharycar/catalogue/persistence/Reservation.java
@@ -1,4 +1,62 @@
 package sharycar.catalogue.persistence;
+import javax.persistence.*;
+
 
 public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false)
+    private String carId;
+
+    @Column(nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String fromDateTime;
+
+    private String toDateTime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCarId() {
+        return carId;
+    }
+
+    public void setCarId(String carId) {
+        this.carId = carId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFromDateTime() {
+        return fromDateTime;
+    }
+
+    public void setFromDateTime(String fromDateTime) {
+        this.fromDateTime = fromDateTime;
+    }
+
+    public String getToDateTime() {
+        return toDateTime;
+    }
+
+    public void setToDateTime(String toDateTime) {
+        this.toDateTime = toDateTime;
+    }
 }
+
